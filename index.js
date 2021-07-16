65d4bb975d9f42c63f44c8dc0ca3d53ee0e938fd
luis cortazar <lcortazar4@gmail.com>
2021-07-16 09:32:49 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+main.remove();
+
+ const newHeader = document.createElement('h1');
+
+ newHeader.id = 'victory';
+
+ newHeader.innerHTML = 'Luis is the champion';
+
+document.body.appendChild(newHeader);
+
+
+
+
+
+
